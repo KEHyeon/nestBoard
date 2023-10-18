cd9924e67d8d74a77396928e3baa3c6690e4f73d
KEHhyeon <kth050213@naver.com>
2023-10-18 16:13:03 +0900

    Feat: createComment Service

--- src/board/comment.service.ts
+++ src/board/comment.service.ts
@@ -2,11 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Board } from './entities/board.entity';
+import { CreateCommentdDto } from './dtos/comment/create-comment.dto';
+import { BoardService } from './board.service';
 
 @Injectable()
 export class CommentService {
   constructor(
     @InjectRepository(Comment) private commentRepo: Repository<Comment>,
     @InjectRepository(Board) private boardRepo: Repository<Comment>,
+    private boardService: BoardService,
   ) {}
+
+  async create(boardId: number, createCommentDto: CreateCommentdDto) {
+    const board = await this.boardService.findOne(boardId);
+    const comment = await this.commentRepo.create({
+      ...createCommentDto,
+      board,
+    });
+    return await this.commentRepo.save(comment);
+  }
 }
--- src/board/dtos/comment/create-comment.dto.ts
+++ src/board/dtos/comment/create-comment.dto.ts
@@ -1,6 +1,6 @@
 import { IsString } from 'class-validator';
 
-export class CreateBoardDto {
+export class CreateCommentdDto {
   @IsString()
   author: string;
 
